e48582e50b35ad72fb253accec3fe941b680d0a5
nkilm <nikhilmohitelhs@gmail.com>
2022-02-28 00:48:01 +0530

    storing info in mongodb and reading from db

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,40 @@
 const express = require("express");
-const jsonfile = require("jsonfile");
+const MongoClient = require('mongodb').MongoClient;
+
+const URL = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const router = express.Router();
 
-const data = jsonfile.readFileSync("./data/hindiQuotes.json")
-
-router.get("/",(req,res)=>{
-    res.json(data[Math.floor(Math.random()*data.length)]);
+router.get("/", (req, res) => {
+    MongoClient.connect(URL, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db("Hindi_Quotes_REST_API");
+        dbo.collection("hindiQuotes").aggregate([
+            { $sample: { size: 1 } }
+        ]).toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
 })
 
-router.get("/:type",(req,res)=>{
-    res.send(req.params.type);
+router.get("/:type", (req, res) => {
+    MongoClient.connect(URL, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db("Hindi_Quotes_REST_API");
+
+        dbo.collection("hindiQuotes")
+            .aggregate([
+                { $match: { type: req.params.type } },
+                { $sample: { size: 1 } }
+            ]).
+            toArray(function (err, result) {
+                if (err) throw err;
+                res.json(result);
+                db.close();
+            });
+    });
 })
 
 module.exports = router
