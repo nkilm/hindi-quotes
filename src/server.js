39323b85981737bee0e4fa1746c4f3cc93a978a1
nkilm <nikhilmohitelhs@gmail.com>
2022-03-11 22:36:27 +0530

    css load done successfully

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const express = require("express");
-
+const path = require("path");
 const PORT = process.env.PORT | 6060;
 const app = express();
+
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/hindiquotes",(req,res)=>{
     res.sendFile(__dirname+"/index.html")
